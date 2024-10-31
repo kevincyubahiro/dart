18a3b088c67553b683431bbcd83ff165a785ddaa
kevincyubahiro <kevincyubahiro250@gmail.com>
2024-10-31 01:41:04 -0700

    for beginners

---
+++ hello.dart
@@ -0,0 +1,4 @@
+void main(){
+  var name="kevin";
+ print("hello $name welcome to this page");
+}
---
+++ sum.dart
@@ -0,0 +1,6 @@
+void main() {
+  int number1 =20;
+  int number2 =20;
+  int sum = number1 + number2;
+  print('The sum of two number $number1 and $number2 is $sum.');
+}
